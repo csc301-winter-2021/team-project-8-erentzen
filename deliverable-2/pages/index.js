348e04619af85b9acf97fcb79bb77fc6ffdbce45
Chen Yang Chou <c.chou@mail.utoronto.ca>
2021-02-28 12:16:57 +0800

    confidugred basic UI with polaris

--- deliverable-2/pages/index.js
+++ deliverable-2/pages/index.js
@@ -1,7 +1,13 @@
+import { Layout, Page, TextStyle } from '@shopify/polaris';
+
 const Index = () => (
-    <div>
-      <p>Sample app using React and Next.js</p>
-    </div>
+    <Page>
+      <Layout>
+        <TextStyle variation="positive">
+          Sample app using React and Next.js
+        </TextStyle>
+      </Layout>
+    </Page>
   );
   
   export default Index;
