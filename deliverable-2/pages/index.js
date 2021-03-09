dc28876b803c1d5e271c727add6d19a732e58230
ShirleyofHuang <shirley.huang2000@outlook.com>
2021-03-08 23:46:02 -0700

    testing navbars

--- deliverable-2/pages/index.js
+++ deliverable-2/pages/index.js
@@ -1,11 +1,14 @@
 import { Layout, Page, TextStyle } from '@shopify/polaris';
+import NavTabs from './NavTabs';
+import Nav from './NavBar';
 
 const Index = () => (
     <Page>
       <Layout>
         <TextStyle variation="positive">
-          Sample app using React and Next.js. HI!
+          {/* Sample app using React and Next.js. HI! */}
         </TextStyle>
+        <NavTabs></NavTabs>
       </Layout>
     </Page>
   );
