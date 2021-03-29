2ee98416f132eb47bf775032fe1c671d39a5164d
LeshiYang <yanglesh666@gmail.com>
2021-03-29 19:47:03 +0800

    switch branch cache

--- deliverable-2/pages/NavTabs.js
+++ deliverable-2/pages/NavTabs.js
@@ -1,72 +1,8 @@
-// import React, { useState , useCallback }from 'react';
-// import { Layout, Page, Tabs, Card, DataTable, Button, TextField, Icon } from '@shopify/polaris';
-// import Dashboard from './Dashboard';
-// import {InventoryUpdate} from './InventoryUpdate';
-// import {Inventory} from './inventory';
-// // import '@shopify/polaris/styles.css';
-
-// export default class NavTabs extends React.Component {
-//     state = {};
-//     render() {
-//         return (
-//             <Page fullWidth>
-//                 <Layout >
-//                     <TabsExample></TabsExample>
-//                 </Layout>
-//             </Page>
-//         )
-//     }
-// };
-
-// function TabsExample() {
-//     const [selected, setSelected] = useState(0);
-//     const handleTabChange = useCallback(
-//       (selectedTabIndex) => setSelected(selectedTabIndex),
-//       [],
-//     );
-//     const displaySection = (selected) => {
-//         if (selected === 0) {
-//             return <Dashboard></Dashboard>
-//         } else if(selected === 2) {
-//             return <InventoryUpdate></InventoryUpdate>
-//         } else if (selected === 1) {
-//             return <Inventory></Inventory>
-//         }
-//     }
-//     const tabs = [
-//       {
-//         id: 'Dashboard',
-//         content: 'Dashboard',
-//         accessibilityLabel: 'Dashboard',
-//         panelID: 'Dashboard',
-//       },
-//       {
-//         id: 'Inventory',
-//         content: 'Inventory',
-//         panelID: 'Inventory',
-//       },
-//       {
-//         id: 'InventoryUpdates',
-//         content: 'Inventory Updates',
-//         panelID: 'InventoryUpdates',
-//       }
-//     ];
-//     return (
-//       <Card>
-//         <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
-//           <Card.Section>
-//             {displaySection(selected)}
-//           </Card.Section>
-//         </Tabs>
-//       </Card>
-//     );
-//   }
-
 import React, { useState , useCallback }from 'react';
 import { Layout, Page, Tabs, Card, DataTable, Button, TextField, Icon } from '@shopify/polaris';
-import Dashboard from './Dashboard';
-import Order from './Order';
-import Inventory from './inventory';
+import {Dashboard} from './Dashboard';
+import {InventoryUpdate} from './InventoryUpdate';
+import {Inventory} from './inventory';
 // import '@shopify/polaris/styles.css';
 
 export default class NavTabs extends React.Component {
@@ -92,7 +28,7 @@ function TabsExample() {
         if (selected === 0) {
             return <Dashboard></Dashboard>
         } else if(selected === 2) {
-            return <Order></Order>
+            return <InventoryUpdate></InventoryUpdate>
         } else if (selected === 1) {
             return <Inventory></Inventory>
         }
@@ -110,9 +46,9 @@ function TabsExample() {
         panelID: 'Inventory',
       },
       {
-        id: 'Orders',
-        content: 'Orders',
-        panelID: 'Orders',
+        id: 'InventoryUpdates',
+        content: 'Inventory Updates',
+        panelID: 'InventoryUpdates',
       }
     ];
     return (
@@ -125,6 +61,5 @@ function TabsExample() {
       </Card>
     );
   }
-
 
 
