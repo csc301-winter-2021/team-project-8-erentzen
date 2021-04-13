8a1e82afd83f32499f825fb4b8c88789f31d3b8d
LeshiYang <yanglesh666@gmail.com>
2021-04-14 06:39:08 +0800

    link get order to shopify store

--- deliverable-2/items_backend/items.service.js
+++ deliverable-2/items_backend/items.service.js
@@ -1,59 +1,3 @@
-// const mysql = require('mysql2');
-// const con = require('./connection').con
-
-// module.exports = {
-//     getAll,
-//     updateInventory
-// }
-
-// async function getAll() {
-//     const select = `SELECT erentzen.variant.variant_id, item_name, description, price, stock, comleted as completed, quantity
-//     FROM erentzen.item JOIN erentzen.variant ON erentzen.item.item_id = erentzen.variant.item_id
-//     LEFT JOIN erentzen.item_order ON erentzen.variant.variant_id = erentzen.item_order.variant_id;`;
-//     const query = await con.promise().query(select);
-//     const result = query[0];
-//     rows = []
-//     for (i = 0; i < result.length; i++) {
-//       pending = 0
-//       if (result[i].quantity != null) {
-//         if (result[i].completed != null) {
-//           pending = result[i].quantity - result[i].completed
-//         } else {
-//           pending = result[i].quantity
-//         }
-//       } else {
-//         pending = 0
-//       }
-//       item = [
-//           result[i].variant_id,
-//           result[i].item_name,
-//           result[i].description,
-//           result[i].stock,
-//           pending,
-//           result[i].price
-//         ]
-//       rows.push(item)
-//     }
-//     return rows
-// }
-
-// async function updateInventory(id, count) {
-//   const sql = `update erentzen.variant set stock = ${count} where variant_id = ${id};`
-//   await con.promise().query(sql);
-//   return "Update Success"
-// }
-
-// getAll(function(err,data){
-//   if (err) {
-//       // error handling code goes here
-//       console.log("ERROR : ",err);            
-//   } else {            
-//       // code to execute on data retrieval
-//       console.log("result from db is : ",data);
-//       return data   
-//   }    
-// });
-
 const mysql = require('mysql2');
 const con = require('./connection').con
 
@@ -110,7 +54,21 @@ async function getAll(store) {
 }
 
 async function getRecentOrder(){
-    const select = `SELECT order_id, item_name, description, quantity, data_time  from erentzen.variant JOIN erentzen.item_order ON erentzen.variant.variant_id = erentzen.item_order.variant_id LEFT JOIN erentzen.item ON erentzen.variant.item_id = erentzen.item.item_id;`;
+  const findStore = `SELECT * FROM erentzen.store
+  WHERE store_name = "${store}";`;
+  const storeQuery = await con.promise().query(findStore);
+  const storeResult = storeQuery[0];
+  if (storeResult.length <= 0) {
+    // error no store found
+    console.log("no such store exists")
+  }else{
+    let store_id = storeResult[0].store_id;
+    const select = `SELECT order_id, item_name, description, quantity, data_time  
+    FROM erentzen.variant 
+    JOIN erentzen.item_order 
+    ON erentzen.variant.variant_id = erentzen.item_order.variant_id 
+    LEFT JOIN erentzen.item ON erentzen.variant.item_id = erentzen.item.item_id
+    WHERE erentzen.item.store_id = ${store_id};`;
     const query = await con.promise().query(select);
     const result = query[0];
     rows = []
@@ -125,6 +83,8 @@ async function getRecentOrder(){
       rows.push(order)
     }
     return rows
+  }
+    
 }
 
 async function updateInventory(id, count) {
