30621c3ed27053aab534463ef1f466c68124519b
ShirleyofHuang <shirley.huang2000@outlook.com>
2021-04-14 09:44:22 -0600

    s

--- deliverable-2/components/InventoryUpdate.js
+++ deliverable-2/components/InventoryUpdate.js
@@ -199,6 +199,7 @@ function UpdateForm(props) {
                 <Stack distribution="trailing">
                     <Button onClick={onClick} primary>Add</Button>
                 </Stack>
+                
             </FormLayout>
 
         </Card>
